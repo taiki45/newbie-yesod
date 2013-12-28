528a05ebe4982e909327f270fa0809c6c4ec3596
Taiki ONO <taiks.4559@gmail.com>
2013-12-29 03:55:45 +0900

    Add article page

--- Handler/Blog.hs
+++ Handler/Blog.hs
@@ -1,9 +1,24 @@
-module Handler.Blog where
+module Handler.Blog
+    ( getBlogR
+    , postBlogR
+    )
+where
 
 import Import
+import Yesod.Form.Nic (YesodNic, nicHtmlField)
+instance YesodNic App
 
 getBlogR :: Handler Html
-getBlogR = error "Not yet implemented: getBlogR"
+getBlogR = do
+    articles <- runDB $ selectList [] [Desc ArticleTitle]
+    (articleWidget, enctype) <- generateFormPost entryForm
+    defaultLayout $ do
+        $(widgetFile "articles")
 
 postBlogR :: Handler Html
 postBlogR = error "Not yet implemented: postBlogR"
+
+entryForm :: Form Article
+entryForm = renderDivs $ Article
+    <$> areq textField "Title" Nothing
+    <*> areq nicHtmlField "Content" Nothing
